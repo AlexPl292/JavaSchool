947c6ba99a2665eef4bf97390510909b5f72d315
Alex Plate <AlexPl292@gmail.com>
2016-09-13 20:07:20 +0300

    Unregister sql driver and destroy entityManagerFactory on context destroying

--- src/main/java/com/tsystems/javaschool/util/EMU.java
+++ src/main/java/com/tsystems/javaschool/util/EMU.java
@@ -9,6 +9,10 @@ import javax.persistence.Persistence;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
 
 /**
  * Created by alex on 07.09.16.
@@ -62,6 +66,29 @@ public class EMU implements ServletContextListener {
 
     @Override
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        logger.info("Context destroyed");
+        // ... First close any background tasks which may be using the DB ...
+        // ... Then close any DB connection pools ...
+
+        // Now deregister JDBC drivers in this context's ClassLoader:
+        // Get the webapp's ClassLoader
+        EMU.closeEntityManagerFactory();
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        // Loop through all drivers
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            if (driver.getClass().getClassLoader() == cl) {
+                // This driver was registered by the webapp's ClassLoader, so deregister it:
+                try {
+                    logger.info("Deregistering JDBC driver");
+                    DriverManager.deregisterDriver(driver);
+                } catch (SQLException ex) {
+                    logger.error("Error deregistering JDBC driver", ex);
+                }
+            } else {
+                // driver was not registered by the webapp's ClassLoader and may be in use elsewhere
+                logger.trace("Not deregistering JDBC driver as it does not belong to this webapp's ClassLoader");
+            }
+        }
     }
 }
