0e918bc169ac56c4b07bd34551bf8b412e32c162
Alex Plate <AlexPl292@gmail.com>
2016-09-27 03:45:19 +0300

    Add contracts rest controller

---
+++ src/main/java/com/tsystems/javaschool/controllers/ContractRest.java
@@ -0,0 +1,32 @@
+package com.tsystems.javaschool.controllers;
+
+import com.tsystems.javaschool.business.dto.ContractDto;
+import com.tsystems.javaschool.business.services.interfaces.ContractService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by alex on 27.09.16.
+ */
+@RestController
+@RequestMapping("/rest/contract")
+public class ContractRest {
+
+    private final transient ContractService service;
+    private static final Logger logger = Logger.getLogger(CustomerRest.class);
+
+    @Autowired
+    public ContractRest(ContractService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<ContractDto> loadAll() {
+        return service.loadAll();
+    }
+}
