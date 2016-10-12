b3581801fd6d05070964594a362caeb0b8db6448
Alex Plate <AlexPl292@gmail.com>
2016-10-12 17:51:24 +0300

    Add request for roles

--- src/main/java/com/tsystems/javaschool/controllers/rest/UserRest.java
+++ src/main/java/com/tsystems/javaschool/controllers/rest/UserRest.java
@@ -5,7 +5,12 @@ import com.tsystems.javaschool.util.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by alex on 06.10.16.
@@ -96,5 +101,11 @@ public class UserRest {
         } else {
             return ResponseEntity.ok().body("");
         }
+    }
+
+    @GetMapping("/role")
+    public ResponseEntity getRole() {
+        List<SimpleGrantedAuthority> authorities = (List<SimpleGrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        return ResponseEntity.ok().body(authorities);
     }
 }
